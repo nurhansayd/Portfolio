623b872eda9c778f95e423c0e085f86fe422db16
Nurhan Abd Elghafar <Nurhan.abdelghafar@CARI-TECH.com>
2023-11-30 15:30:38 +0200

    portfolio v3

--- src/data.jsx
+++ src/data.jsx
@@ -22,8 +22,8 @@ import strapiImg from './assets/images/strapi.png';
 
 export const links = [
   { id: nanoid(), href: '#hero', text: 'home' },
-  { id: nanoid(), href: '#skills', text: 'skills' },
   { id: nanoid(), href: '#about', text: 'about' },
+  { id: nanoid(), href: '#skills', text: 'skills' },
   { id: nanoid(), href: '#projects', text: 'projects' },
 
 ];
